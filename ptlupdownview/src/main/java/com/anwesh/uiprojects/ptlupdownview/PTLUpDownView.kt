8c41f69f5316db256e93868128ad8798d0bc17d4
Anwesh43 <anweshthecool0@gmail.com>
2018-09-07 01:17:50 +0530

    canvas drawPTLUDNode extension methid

--- ptlupdownview/src/main/java/com/anwesh/uiprojects/ptlupdownview/PTLUpDownView.kt
+++ ptlupdownview/src/main/java/com/anwesh/uiprojects/ptlupdownview/PTLUpDownView.kt
@@ -9,8 +9,32 @@ import android.view.MotionEvent
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.content.Context
+import android.graphics.Color
 
 val nodes : Int = 5
+
+fun Canvas.drawPTLUDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = (w / (nodes + 1))
+    val index : Int = i % 2
+    val sf : Float = 1f - 2 * index
+    val size : Float = gap / 3
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(scale -0.5f, 0f)) * 2
+    paint.color = Color.WHITE
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap/4 + gap/2 + gap * i, h/2 + (h/2 - size/2) * sc2 * sf)
+    for (j in 0..1) {
+        save()
+        rotate(90f * (1 - sc1) * sf)
+        drawLine(-size/2, 0f, size/2, 0f, paint)
+        restore()
+    }
+    restore()
+}
 
 class PTLUpDownView(ctx : Context) : View(ctx) {
 
