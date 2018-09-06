f116b17d0f59f42c7dd5247ece354145cd2b0c73
Anwesh43 <anweshthecool0@gmail.com>
2018-09-07 00:55:54 +0530

    adding libraries and defining number of nodes

--- ptlupdownview/src/main/java/com/anwesh/uiprojects/ptlupdownview/PTLUpDownView.kt
+++ ptlupdownview/src/main/java/com/anwesh/uiprojects/ptlupdownview/PTLUpDownView.kt
@@ -4,3 +4,10 @@ package com.anwesh.uiprojects.ptlupdownview
  * Created by anweshmishra on 07/09/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5 
