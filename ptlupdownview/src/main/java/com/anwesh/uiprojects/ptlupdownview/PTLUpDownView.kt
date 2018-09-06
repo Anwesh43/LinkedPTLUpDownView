2c809d7561f4abeb348887759f1a6df5898d017e
Anwesh43 <anweshthecool0@gmail.com>
2018-09-07 00:58:33 +0530

    adding PTLUpDownView class

--- ptlupdownview/src/main/java/com/anwesh/uiprojects/ptlupdownview/PTLUpDownView.kt
+++ ptlupdownview/src/main/java/com/anwesh/uiprojects/ptlupdownview/PTLUpDownView.kt
@@ -10,4 +10,22 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.content.Context
 
-val nodes : Int = 5 
+val nodes : Int = 5
+
+class PTLUpDownView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
