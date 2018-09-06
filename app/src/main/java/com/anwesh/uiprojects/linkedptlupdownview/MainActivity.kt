ee016cb9c27380a00b60c9ed7f4beaf5ac3d8f49
Anwesh43 <anweshthecool0@gmail.com>
2018-09-07 01:49:03 +0530

    adding PTLUpDownView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedptlupdownview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedptlupdownview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedptlupdownview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ptlupdownview.PTLUpDownView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PTLUpDownView.create(this)
     }
 }
